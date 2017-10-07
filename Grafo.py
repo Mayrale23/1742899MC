67a7fefc2c42ac5e5d82594caf6148fc1274c744
Mayrale23 <30917378+Mayrale23@users.noreply.github.com>
2017-10-06 20:48:48 -0500

    Create Grafo.py

---
+++ Grafo.py
@@ -0,0 +1,18 @@
+class Grafo:
+ 
+    def __init__(self):
+        self.M = set() 
+        self.R = dict() 
+        self.vecinos = dict() 
+ 
+    def agregar(self, m):
+        self.M.add(m)
+        if not m in self.vecinos: 
+            self.vecinos[m] = set() 
+ 
+    def conect(self, m, a, peso=1):
+        self.agrega(m)
+        self.agrega(a)
+        self.E[(m, a)] = self.E[(a, m)] = peso 
+        self.vecinos[m].add(a)
+        self.vecinos[a].add(m)
